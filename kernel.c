b881a2901e76b5feaf4e14f0550b9b3985717755
yuyln <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>
2022-02-11 18:46:46 -0300

    gpu :)

--- kernel.c
+++ kernel.c
@@ -8,28 +8,28 @@ kernel void ShowLGPU(global RGBA *rgba, global LineSegment2D *lines,
                      const int w, const int h, const int nlines, const double lw)
 {
     int I = get_global_id(0);
-    int j = (I % w) - w / 2;
-    int i = (I - j) / w - h / 2;
+    int x = (I % w) - w / 2;
+    int y = (I - x) / w - h / 2;
     double dummy, dummy2;
-    Show(lines, nlines, lw, i, j, &dummy, &dummy2, &rgba[I]);
+    Show(lines, nlines, lw, x, y, &dummy, &dummy2, &rgba[I]);
 }
 
 kernel void ShowCGPU(global RGBA *rgba, global LineSegment2D *lines, const Vec4 col, 
                      const int w, const int h, const int nlines, const double lw)
 {
     int I = get_global_id(0);
-    int j = (I % w) - w / 2;
-    int i = (I - j) / w - h / 2;
+    int x = (I % w) - w / 2;
+    int y = (I - x) / w - h / 2;
     double dummy, dummy2;
-    ShowC(lines, nlines, lw, i, j, &dummy, &dummy2, &rgba[I], col);
+    ShowC(lines, nlines, lw, x, y, &dummy, &dummy2, &rgba[I], col);
 }
 
 kernel void ShowGGPU(global RGBA *rgba, global LineSegment2D *lines, const Vec4 Gcol,
                      const int w, const int h, const int nlines, const double lw, const double pR)
 {
     int I = get_global_id(0);
-    int j = (I % w) - w / 2;
-    int i = (I - j) / w - h / 2;
+    int x = (I % w) - w / 2;
+    int y = (I - x) / w - h / 2;
     double dummy, dummy2;
-    ShowG(lines, nlines, lw, i, j, &dummy, &dummy2, &rgba[I], pR, Gcol);
+    ShowG(lines, nlines, lw, x, y, &dummy, &dummy2, &rgba[I], pR, Gcol);
 }
--- maingpu.cpp
+++ maingpu.cpp
@@ -34,7 +34,6 @@ int main()
     int h = 1080;
     int T = w * h;
 
-
     char *kernel;
     ReadFile("kernel.c", &kernel);
 
@@ -50,6 +49,85 @@ int main()
 
     InitGlobalWorkItems(1, &T, &globalwork);
     InitGroupWorkItemsGCD(1, &T, &localwork, devices);
+
+    RGBA *rgbas = new RGBA[T];
+    RGBA *rgbas2 = new RGBA[T];
+    RGBA32 *pixels = new RGBA32[T];
+    Curve seno, sin2;
+    cl_mem senob, sin2b;
+    cl_mem RGBAsb, RGBAcb;
+
+    InitCurve(&seno, 100);
+    InitCurve(&sin2, 100);
+    Sin(seno, h / 2, 1.0 / w, 0.0, -w / 2.0, w / 2.0);
+    Sin(sin2, -h / 2, 1.0 / w, 0.0, -w / 2.0, w / 2.0);
+
+    CreateBuffer(&senob, seno.lines, sizeof(LineSegment2D) * seno.nLines, context, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR);
+    WriteBuffer(&senob, seno.lines, sizeof(LineSegment2D) * seno.nLines, queue);
+    CreateBuffer(&sin2b, sin2.lines, sizeof(LineSegment2D) * sin2.nLines, context, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR);
+    WriteBuffer(&sin2b, sin2.lines, sizeof(LineSegment2D) * sin2.nLines, queue);
+
+
+    rgbas = new RGBA[T];
+    rgbas2 = new RGBA[T];
+    CreateBuffer(&RGBAsb, rgbas, sizeof(RGBA) * T, context, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR);
+    CreateBuffer(&RGBAcb, rgbas2, sizeof(RGBA) * T, context, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR);
+    for (int j = 0; j < T; j++)
+    {
+        rgbas[j] = {{1.0, 1.0, 1.0, 1.0}};
+        rgbas2[j] = {{1.0, 1.0, 1.0, 1.0}};
+    }
+    WriteBuffer(&RGBAsb, rgbas, sizeof(RGBA) * T, queue);
+    WriteBuffer(&RGBAcb, rgbas2, sizeof(RGBA) * T, queue);
+
+
+    Vec4 col = {{0.0, 0b10011000, 255.0, 0.0}};
+    col = Vec4MultScalar(col, 1.0 / 255.0);
+    double pR = 100.0;
+    double lw = 6.0;
+
+    SetKernelArg(&kernels[2], &RGBAsb, sizeof(cl_mem), 0);
+    SetKernelArg(&kernels[2], &senob, sizeof(cl_mem), 1);
+    SetKernelArg(&kernels[2], &col, sizeof(Vec4), 2);
+    SetKernelArg(&kernels[2], &w, sizeof(int), 3);
+    SetKernelArg(&kernels[2], &h, sizeof(int), 4);
+    SetKernelArg(&kernels[2], &seno.nLines, sizeof(int), 5);
+    SetKernelArg(&kernels[2], &lw, sizeof(double), 6);
+    SetKernelArg(&kernels[2], &pR, sizeof(double), 7);
+
+    EnqueueND(&queue, &kernels[2], 1, NULL, globalwork, localwork);
+
+    col = {{0xFF, 0x67, 0.0, 0.0}};
+    col = Vec4MultScalar(col, 1.0 / 255.0);
+    pR = 100.0;
+    lw = 6.0;
+
+    SetKernelArg(&kernels[2], &RGBAcb, sizeof(cl_mem), 0);
+    SetKernelArg(&kernels[2], &sin2b, sizeof(cl_mem), 1);
+    SetKernelArg(&kernels[2], &col, sizeof(Vec4), 2);
+    SetKernelArg(&kernels[2], &w, sizeof(int), 3);
+    SetKernelArg(&kernels[2], &h, sizeof(int), 4);
+    SetKernelArg(&kernels[2], &sin2.nLines, sizeof(int), 5);
+    SetKernelArg(&kernels[2], &lw, sizeof(double), 6);
+    SetKernelArg(&kernels[2], &pR, sizeof(double), 7);
+    
+    EnqueueND(&queue, &kernels[2], 1, NULL, globalwork, localwork);
+
+    Finish(&queue);
+
+    ReadBuffer(&RGBAsb, rgbas, sizeof(RGBA) * T, queue);
+    ReadBuffer(&RGBAcb, rgbas2, sizeof(RGBA) * T, queue);
+
+    for (int i = 0; i < T; i++)
+    {
+        RGBA s = Vec4Sum(Vec4FromScalar(0.0), rgbas[i]);
+        s = Vec4Mult(s, rgbas2[i]);
+        s = Vec4PowS(s, 1.0 / 2.2);
+        pixels[i] = (ToRGBA32V4(s)) | 0xFF000000;
+    }
+
+    stbi_write_png("./imgs/outG.png", w, h, 4, pixels, w * sizeof(RGBA32));
     return 0;
 }
+
 
